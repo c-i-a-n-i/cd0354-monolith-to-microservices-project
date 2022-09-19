1edce81ee5e894b15a7eaf6d074a099ebc272952
Chidubem Ani <ifeatu96@gmail.com>
2022-09-19 15:44:13 -0700

    move to microservices working

---
+++ setenv.bat
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=postgres
+set POSTGRES_HOST=database-1.cjbpr2mhfaho.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET=udagram-842129021188-dev
+set AWS_REGION=us-west-2
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
